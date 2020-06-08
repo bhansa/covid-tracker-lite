85c4e4c30f196c4acd9017b6b326d427f4397404
bhansa <bsaraswat@deloitte.com>
2020-06-08 15:19:14 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// fetch covid data and later set it object cards
+
+let countriesData = null;
+
+let elementIds = {
+  confirmed: "confirmed",
+  active: "active",
+  recovered: "recovered",
+};
+
+function fetchData() {
+  // use js fetch
+  const apiURL = "https://api.covid19india.org/data.json";
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((data) => {
+      let summaryData = data.statewise[0];
+      console.log(summaryData);
+      document.getElementById(elementIds.confirmed).textContent =
+        summaryData.confirmed;
+      document.getElementById(elementIds.active).textContent =
+        summaryData.active;
+      document.getElementById(elementIds.recovered).textContent =
+        summaryData.recovered;
+    })
+    .catch((error) => console.log("api data fetch error", error));
+}
+
+fetchData();
---
+++ sw.js
@@ -0,0 +1,33 @@
+self.importScripts(
+  "https://cdnjs.cloudflare.com/ajax/libs/sw-toolbox/3.6.1/sw-toolbox.js"
+);
+
+self.toolbox.precache([
+  "/index.html",
+  "/main.js",
+  "/main.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
+]);
+
+self.toolbox.router.get("/(.*)", function (request, values, options) {
+  return toolbox.networkFirst(request, values, options).catch(function (error) {
+    if (
+      request.method === "GET" &&
+      request.headers.get("accept").includes("text/html")
+    ) {
+      return toolbox.cacheOnly(new Request("/index.html"), values, options);
+    }
+    throw error;
+  });
+});
+
+// sw activated
+self.addEventListener("activate", function (event) {
+  // claims the sw for client after wait skipped
+  return self.clients.claim();
+});
+
+self.addEventListener("install", (event) => {
+  // skip wait to install a new version of service worker
+  self.skipWaiting();
+});
