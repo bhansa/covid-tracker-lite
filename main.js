b6f2f36908532f9c6f948cc320beeb9b5e151d89
bhansa <bsaraswat@deloitte.com>
2020-06-08 18:37:34 +0530

    updated js functions

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-// fetch covid data and later set it object cards
+// fetch covid data and later set it to object cards
+(function () {
+  let countriesData = null;
 
-let countriesData = null;
+  let elementIds = {
+    confirmed: "confirmed",
+    active: "active",
+    recovered: "recovered",
+  };
 
-let elementIds = {
-  confirmed: "confirmed",
-  active: "active",
-  recovered: "recovered",
-};
+  function fetchData() {
+    // use js fetch
+    const apiURL = "https://api.covid19india.org/data.json";
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((data) => {
+        let summaryData = data.statewise[0];
+        document.getElementById(elementIds.confirmed).textContent =
+          summaryData.confirmed;
+        document.getElementById(elementIds.active).textContent =
+          summaryData.active;
+        document.getElementById(elementIds.recovered).textContent =
+          summaryData.recovered;
+      })
+      .catch((error) => console.log("api data fetch error", error));
+  }
 
-function fetchData() {
-  // use js fetch
-  const apiURL = "https://api.covid19india.org/data.json";
-  fetch(apiURL)
-    .then((response) => response.json())
-    .then((data) => {
-      let summaryData = data.statewise[0];
-      document.getElementById(elementIds.confirmed).textContent =
-        summaryData.confirmed;
-      document.getElementById(elementIds.active).textContent =
-        summaryData.active;
-      document.getElementById(elementIds.recovered).textContent =
-        summaryData.recovered;
-    })
-    .catch((error) => console.log("api data fetch error", error));
-}
-
-fetchData();
+  fetchData();
+})();
