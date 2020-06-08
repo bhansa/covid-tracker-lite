ae1b7b1b0007611e01bbe2c2e49c459af6e57f05
bhansa <bsaraswat45@gmail.com>
2020-06-09 02:17:29 +0530

    persist dark theme toggle

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,13 +1,29 @@
 // theme toggle function, toggles through light and dark mode
 (function () {
   let themeToggle = document.getElementById("theme-toggle");
-  themeToggle.onclick = function (event) {
-    if (event.target.checked) {
-      document.documentElement.setAttribute("data-theme", "dark");
-      localStorage.setItem("data-theme", "dark");
-    } else {
-      document.documentElement.setAttribute("data-theme", "light");
-      localStorage.setItem("data-theme", "light");
+  themeToggle.addEventListener(
+    "click",
+    function (event) {
+      if (event.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("data-theme", "dark");
+      } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("data-theme", "light");
+      }
+    },
+    false
+  );
+
+  const currentTheme = localStorage.getItem("data-theme")
+    ? localStorage.getItem("data-theme")
+    : null;
+
+  if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "dark") {
+      themeToggle.checked = true;
     }
-  };
+  }
 })();
